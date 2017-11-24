d76336d233059696467df19f66c151d73b8faa31
zemla4ok <dima-vital@live.com>
2017-11-24 23:42:01 +0300

    adding apartments

--- hotelClient/hotelClient/WorkWindow.xaml.cs
+++ hotelClient/hotelClient/WorkWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -32,21 +33,70 @@ namespace hotelClient
 
         private void AddApartment(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                if(Validator.ValidTextBoxes(this.CloseDate.Text))
+                if (Validator.ValidTextBoxes(this.CloseDate.Text))
                 {
+                    using (SqlConnection cn = Connector.GetConnection())
+                    {
+                        cn.Open();
+                        SqlCommand cmd = new SqlCommand("AddApartment", cn);
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
 
+
+                        SqlParameter currCost = new SqlParameter();
+                        currCost.ParameterName = "@curr_cost";
+                        currCost.Value = this.CurrCost.Text;
+
+                        SqlParameter places = new SqlParameter();
+                        places.ParameterName = "@places";
+                        places.Value = this.Places.Text;
+
+                        SqlParameter freePlaces = new SqlParameter();
+                        freePlaces.ParameterName = "@free_places";
+                        freePlaces.Value = this.FreePlaces.Text;
+
+                        SqlParameter hotel = new SqlParameter();
+                        hotel.ParameterName = "@hotel";
+                        hotel.Value = this.HotelName;
+
+                        SqlParameter city = new SqlParameter();
+                        city.ParameterName = "@city";
+                        city.Value = this.CityName;
+
+                        SqlParameter apartNum = new SqlParameter();
+                        apartNum.ParameterName = "@apartment_num";
+                        apartNum.Value = this.ApartNum.Text;
+
+                        SqlParameter closeDate = new SqlParameter();
+                        closeDate.ParameterName = "@close_date";
+                        closeDate.Value = this.CloseDate.Text;
+
+                        cmd.Parameters.Add(currCost);
+                        cmd.Parameters.Add(places);
+                        cmd.Parameters.Add(freePlaces);
+                        cmd.Parameters.Add(hotel);
+                        cmd.Parameters.Add(city);
+                        cmd.Parameters.Add(apartNum);
+                        cmd.Parameters.Add(closeDate);
+
+                        SqlParameter rc = new SqlParameter();
+                        rc.ParameterName = "@rc";
+                        rc.SqlDbType = System.Data.SqlDbType.Bit;
+                        rc.Direction = System.Data.ParameterDirection.Output;
+                        cmd.Parameters.Add(rc);
+
+                        cmd.ExecuteNonQuery();
+                        cn.Close();
+
+                        if ((bool)cmd.Parameters["@rc"].Value)
+                            MessageBox.Show("Adding apartments complete");
+                        else
+                            MessageBox.Show("Adding error");
+                    }
                 }
                 else
                 {
                     MessageBox.Show("Input Close date");
                 }
-            }
-            catch(Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
         }
     }
 }
--- insert apartment.sql
+++ insert apartment.sql
@@ -1,25 +1,45 @@
-CREATE PROCEDURE InsertApartment
+CREATE PROCEDURE AddApartment
 	@curr_cost INT,
 	@places INT,
 	@free_places INT,
+	@city NVARCHAR(30),
 	@hotel NVARCHAR(50),
 	@apartment_num INT,
-	@close_date date
+	@close_date date,
+	@rc bit output
 	AS BEGIN
-		DECLARE @hotel_id int;
-		SET @hotel_id = (SELECT ID FROM HOTELS WHERE NAME like @hotel)
-		INSERT INTO APARTMENTS(CURRENT_COST, PLACES, FREE_PLACES, HOTEL_ID, APARTMENTS_NUM, CLOSE_DATE)
-			VALUES(@curr_cost, @places, @free_places, @hotel_id, @apartment_num, @close_date);
+		SET @rc = 0;
+		DECLARE @city_id INT;
+		DECLARE @hotel_id INT;
+		IF @free_places <= @places
+		BEGIN
+			IF EXISTS(SELECT ID FROM CITY WHERE NAME like @city)
+			BEGIN
+				SET @city_id = (SELECT ID FROM CITY WHERE NAME like @city);
+				IF EXISTS(SELECT ID FROM HOTELS WHERE NAME like @hotel and CITY_ID = @city_id)
+				BEGIN
+					SET @hotel_id = (SELECT ID FROM HOTELS WHERE NAME like @hotel and CITY_ID = @city_id);
+					IF NOT EXISTS(SELECT ID FROM APARTMENTS WHERE HOTEL_ID = @hotel_id and APARTMENTS_NUM = @apartment_num)
+					BEGIN
+						INSERT INTO APARTMENTS(CURRENT_COST, PLACES, FREE_PLACES, HOTEL_ID, APARTMENTS_NUM, CLOSE_DATE)
+							VALUES(@curr_cost, @places, @free_places, @hotel_id, @apartment_num, @close_date);
+							SET @rc = 1;
+					END
+				END
+			END
+		END
 	END;
 
-exec InsertApartment
+exec AddApartment
 	@curr_cost = 100,
 	@places = 5,
 	@free_places = 2,
-	@hotel = 'zvezda',
-	@apartment_num = 123,
-	@close_date = '10-11-1997'
+	@hotel = 'slm1',
+	@city = 'slm',
+	@apartment_num = 200,
+	@close_date = '10.11.1997',
+	@rc =1;
 
 select * from APARTMENTS;
 
-drop procedure InsertApartment;
+drop procedure AddApartment;
